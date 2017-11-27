b60ceb2aa34353986b98cf5b238bc6cb609f1039
Leo646 <33529768+Leo646@users.noreply.github.com>
2017-11-26 23:23:53 -0500

    Add files via upload

---
+++ JuegoBlackjack/src/juegoblackjack/BlackjackGUI.java
@@ -0,0 +1,196 @@
+
+package juegoblackjack;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author leonardo
+ */
+public class BlackjackGUI {
+     public static void main(String[] args) {
+   
+      int dinero;          // La cantidad de dinero que tiene el usuario
+      int apuesta;            // Cantidad de apuestas de usuario en un juego.
+      boolean usuarioGandador;   // ¿El usuario gano el juego?
+      
+         JOptionPane.showMessageDialog(null,"Bienbenido al juego de  blackjack.");
+      
+      
+      dinero = 100;  // El usuario comienza con $ 100.
+   
+      while (true) {
+          JOptionPane.showMessageDialog(null,"Tienes " + dinero + " dalares.");
+          do {
+             apuesta=Integer.parseInt(JOptionPane.showInputDialog("¿Cuanto dienero quieres Apostar ?  (Entre  0 y y su cantidad maxima.)"));
+             
+             apuesta = TextIO.getlnInt();
+             
+             
+             if (apuesta < 0 || apuesta > dinero)
+                 JOptionPane.showInputDialog("Tu respuesta de esta entres 0 y" + dinero+ '.');
+          } while (apuesta < 0 || apuesta > dinero);
+          if (apuesta  == 0)
+             break;
+          usuarioGandador = playBlackjack();
+          if (usuarioGandador)
+             dinero = dinero + apuesta;
+          else
+             dinero =dinero - apuesta;
+          
+          if (dinero == 0) {
+             JOptionPane.showMessageDialog(null,"¡Parece que te has quedado sin dinero!");
+             break;
+          }
+      }
+      
+      
+      JOptionPane.showMessageDialog(null,"Te vas con $" + dinero + '.');
+   
+   } // fin del main()
+   
+ /*
+   /**
+    * Permita que el usuario juegue un juego de Blackjack, con la computadora como distribuidor.
+    * @devuelve verdadero si el usuario gana el juego, falso si el usuario pierde.
+    */  
+   static boolean playBlackjack() {
+
+      Baraja baraja;                  // Una baraja de cartas.  Una nueva bajara para cada juego.
+      ManoBlackjack manoDistribuidor;   // La mano del distribuidor.
+      ManoBlackjack manoUsuario;     // La mano del usuario .
+      
+      baraja = new Baraja();
+      manoDistribuidor = new ManoBlackjack();
+      manoUsuario = new ManoBlackjack();
+
+      /*  Baraja el mazo, luego reparte dos cartas a cada jugador. */
+      
+      baraja.barajar();
+      manoDistribuidor.añadirCarta(baraja.cartaOferta());
+      manoDistribuidor.añadirCarta(baraja.cartaOferta());
+      manoUsuario.añadirCarta(baraja.cartaOferta());
+      manoUsuario.añadirCarta(baraja.cartaOferta());
+      
+
+      
+      /* Comprueba si uno de los jugadores tiene Blackjack (dos cartas que suman un total de 21).
+         El jugador con Blackjack gana el juego. El distribuidor gana lazos.
+      */
+      
+      if (manoDistribuidor.getBlackjackValor()== 21) {
+           JOptionPane.showMessageDialog(null,"El distribuidor tiene el " + manoDistribuidor.getCarta(0)
+                                   + " y el " + manoDistribuidor.getCarta(1) + ".");
+           JOptionPane.showMessageDialog(null,"El usuario tiene el " + manoUsuario.getCarta(0)
+                                     + " y el " + manoUsuario.getCarta(1) + ".");
+          
+           JOptionPane.showMessageDialog(null,"El distribuidor tiene Blackjack. El distribuidor gana");
+           return false;
+      }
+      
+      if (manoUsuario.getBlackjackValor()== 21) {
+           JOptionPane.showMessageDialog(null,"El distribuidor tiene el " + manoDistribuidor.getCarta(0)
+                                   + " y el " + manoDistribuidor.getCarta(1) + ".");
+           JOptionPane.showMessageDialog(null,"El usuario tiene el " + manoUsuario.getCarta(0)
+                                     + " y el " + manoUsuario.getCarta(1) + ".");
+          
+           JOptionPane.showMessageDialog(null,"Tienes Blackjack. Tú ganas.");
+           return true;
+      }
+      
+      /*  If neither player has Blackjack, play the game.  First the user 
+          gets a chance to draw cards (i.e., to "Hit").  The while loop ends 
+          when the user chooses to "Stand".  If the user goes over 21,
+          the user loses immediately.
+      */
+      
+      while (true) {
+          
+           /* Display user's cards, and let user decide to Hit or Stand. */
+
+           
+           JOptionPane.showMessageDialog(null,"Tus cartas son:");
+           for ( int i = 0; i < manoUsuario.getCountCarta(); i++ )
+             JOptionPane.showMessageDialog(null,"    " + manoUsuario.getCarta(i));
+           JOptionPane.showMessageDialog(null,"Tu total es " + manoUsuario.getBlackjackValor());
+           
+           JOptionPane.showMessageDialog(null,"El distribuidor muestra el " + manoDistribuidor.getCarta(0));
+           
+           JOptionPane.showInputDialog("Pedir (H) o Plantarse (S)? ");
+         
+           char userAction;  // El usuario responde, 'H' o 'S'.
+           do {
+              userAction = Character.toUpperCase( TextIO.getlnChar() );
+              if (userAction != 'H' && userAction != 'S')
+                 JOptionPane.showInputDialog("Por favor elija H o S:  ");
+           } while (userAction != 'H' && userAction != 'S');
+
+           /* Si el usuario tiene éxito, el usuario obtiene una carta. Si el usuario se planta,
+              el bucle termina (y es el turno del distribuidor de robar cartas).
+           */
+
+           if ( userAction == 'S' ) {
+                   // bucle terminado; el usuario ha terminado de tomar cartas.
+               break;
+           }
+           else {  //userAction es 'H'. Dale una carta al usuario. 
+                   // Si el usuario supera los 21, el usuario pierde.
+               Carta nuevaCarta = baraja.cartaOferta();
+               manoUsuario.añadirCarta(nuevaCarta);
+               
+               JOptionPane.showMessageDialog(null,"Aciertos del usuario.");
+               JOptionPane.showMessageDialog(null,"Tu carta es: " + nuevaCarta);
+               JOptionPane.showMessageDialog(null,"El total que posees ahora es: " + manoUsuario.getBlackjackValor());
+               if (manoUsuario.getBlackjackValor()> 21) {
+                   System.out.println();
+                   JOptionPane.showMessageDialog(null,"Has perdido yendo por encima de 21. Pierdes.");
+                   JOptionPane.showMessageDialog(null,"La otra carta del distribuidor era el: "+ manoDistribuidor.getCarta(1));
+                   return false;  
+               }
+           }
+           
+      } // end del ciclo while
+      
+      /* Si llegamos a este punto, el usuario se planta con 21 o menos.  Ahora es
+          la oportunidad del distribuidor para sacar  El distribuidor roba cartas hasta que el 
+           total sea> 16. Si el distribuidor supera los 21, el este pierde.
+      */
+
+      
+      JOptionPane.showMessageDialog(null,"Usuario plantado");
+      JOptionPane.showMessageDialog(null,"Las cartas del distribuidor son");
+      JOptionPane.showMessageDialog(null,"    " + manoDistribuidor.getCarta(0));
+      JOptionPane.showMessageDialog(null,"    " + manoDistribuidor.getCarta(1));
+      while (manoDistribuidor.getBlackjackValor()<= 16) {
+         Carta nuevaCarta = baraja.cartaOferta();
+         JOptionPane.showMessageDialog(null,"El distribuidor pide y obtiene el " + nuevaCarta);
+         manoDistribuidor.añadirCarta(nuevaCarta);
+         if (manoDistribuidor.getBlackjackValor()> 21) {
+            
+            JOptionPane.showMessageDialog(null,"El distribuidor quiebra por  sobrepasar los 21.  Tu ganas.");
+            return true;
+         }
+      }
+      JOptionPane.showMessageDialog(null,"El distribuidor pose un total de: " + manoDistribuidor.getBlackjackValor());
+      
+      /* Si llegamos a este punto, ambos jugadores tienen 21 o menos. Nosotros
+          puedemos determinar el ganador comparando los valores de sus manos. */
+      
+      
+      if (manoDistribuidor.getBlackjackValor()== manoUsuario.getBlackjackValor()) {
+         JOptionPane.showMessageDialog(null,"El distribuidor gana en un empate. Tú pierdes.");
+         return false;
+      }
+      else if (manoDistribuidor.getBlackjackValor()> manoUsuario.getBlackjackValor()) {
+         JOptionPane.showMessageDialog(null,"El distribuidor gana: " + manoDistribuidor.getBlackjackValor()
+                          + " puntos " + manoUsuario.getBlackjackValor()+ ".");
+         return false;
+      }
+      else {
+         JOptionPane.showMessageDialog(null,"Tu ganas; " + manoUsuario.getBlackjackValor()
+                          + " puntos " + manoDistribuidor.getBlackjackValor()+ ".");
+         return true;
+      }
+
+   }  // fin del juego Blackjack()
+}
---
+++ JuegoBlackjack/src/juegoblackjack/ManoBlackjack.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegoblackjack;
+
+/**
+ *
+ * @author leonardo
+ */
+public class ManoBlackjack extends Mano{
+    /**
+     * Computes and returns the value of this hand in the game
+     * of Blackjack.
+     */
+    public int getBlackjackValor()  {
+
+        int val;      // The value computed for the hand.
+        boolean as;  // This will be set to true if the
+        //   hand contains an ace.
+        int cartas;    // Number of cards in the hand.
+
+        val = 0;
+        as = false;
+        cartas = getCountCarta();  // (method defined in class Hand.)
+
+        for ( int i = 0;  i < cartas;  i++ ) {
+            // Add the value of the i-th card in the hand.
+            Carta carta;    // The i-th card; 
+            int cartaVal;  // The blackjack value of the i-th card.
+            carta = getCarta(i);
+            cartaVal = carta.getValor();  // The normal value, 1 to 13.
+            if (cartaVal > 10) {
+                cartaVal = 10;   // For a Jack, Queen, or King.
+            }
+            if (cartaVal == 1) {
+                as = true;     // There is at least one ace.
+            }
+            val = val + cartaVal;
+        }
+
+        // Now, val is the value of the hand, counting any ace as 1.
+        // If there is an ace, and if changing its value from 1 to 
+        // 11 would leave the score less than or equal to 21,
+        // then do so by adding the extra 10 points to val. 
+
+        if ( as == true  &&  val + 10 <= 21 )
+            val = val + 10;
+
+        return val;
+
+    }  // end getBlackjackValue()
+
+} // end class BlackjackHand
+    
+
